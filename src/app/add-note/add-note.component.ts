1276861e7afd02c19b73f99d933bf430c397c705
Scot Zark <scotzark@gmail.com>
2019-12-17 20:39:24 -0500

    Add task field when saving note.

--- src/app/add-note/add-note.component.ts
+++ src/app/add-note/add-note.component.ts
@@ -4,6 +4,7 @@ import {Note} from "../models/Note";
 import { Observable } from 'rxjs';
 import { NotesResponse } from '../models/NotesResponse';
 import { AddNote } from '../models/AddNote';
+import { Task } from '../models/Task';
 import { v4 as uuid } from 'uuid';
 
 @Component({
@@ -16,6 +17,8 @@ export class AddNoteComponent implements OnInit {
   constructor(private httpClient: HttpClient) { }
 
   model = new Note();
+  task = new Task();
+
   url = "https://todoloop.appspot.com/addNote"
   
 
@@ -35,11 +38,18 @@ export class AddNoteComponent implements OnInit {
     this.model.createTime  = Date.now();
     this.model.modifyTime = Date.now();
     this.model.uuid = "b9496d65-da7b-4c7f-896b-8d7d1cf84a41";
-    console.log(this.model);
+
+    this.task.createTime = this.model.createTime;
+    this.task.modifyTime = this.model.modifyTime;
+    this.task.uuid = this.model.uuid;
+    this.task.taskId = uuid();
+    this.task.parentId = this.model.id;
+
+    console.log(this.task);
 
     var note = new AddNote();
     note.note = this.model;
-    note.tasks = [];
+    note.tasks = [this.task];
 
     return this.httpClient.post<NotesResponse>(this.url, note);
   }
--- src/app/models/Task.ts
+++ src/app/models/Task.ts
@@ -1,10 +1,17 @@
 export class Task {
+    taskId: string;
     parentId: string;
     content: string;
-    
+    title: string;
+    createTime: number;
+    modifyTime: number;
+    syncStatus: string;
+    uuid: string;
+   
     constructor()
     {
-      this.parentId = "";
-      this.content = "";
+      this.title = "";
+      this.syncStatus = "NEEDS_SYNC";
+
     }
   }
